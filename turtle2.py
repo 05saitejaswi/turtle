56c6c110fa6b4b6e7c6613d5fb6003ac6bb5ad3b
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-11-27 16:40:42 +0530

    using loop
    using loop to make square

---
+++ turtle2.py
@@ -0,0 +1,13 @@
+//using loop
+import turtle 
+
+smart = turtle.Turtle()
+
+# Loop 4 times. Everything I want to repeat is 
+# *indented* by four spaces.
+for i in range(4):
+    smart.forward(50)
+    smart.right(90)
+    
+# This isn't indented, so we aren't repeating it.
+turtle.done()
