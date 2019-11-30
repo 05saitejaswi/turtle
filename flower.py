edb8495a99cac543b6c6bb6fa716978ee8a14523
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-11-30 22:06:59 +0530

    flower
    flower using turtle

---
+++ flower.py
@@ -0,0 +1,13 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+my_turtle.speed(0)
+
+def square(length,angle):
+      for i in range(4):
+         my_turtle.forward(length)
+         my_turtle.right(angle)
+for i in range(40):
+   square(100,90)
+   my_turtle.right(10)
+
